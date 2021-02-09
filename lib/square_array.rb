1ada4b94171edbef50b6ea538650179f93be226b
Susana vik <susana.m.vik@gmail.com>
2021-02-09 01:59:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,18 @@
 numbers = [1,2,3]
 
 def square_array(numbers)
+  count = 0
   array = []
-  array << numbers ** 2
+  while count < numbers.length do
+    array << numbers[count]**2
+    count += 1
+  end
   array
 end
 
 p square_array(numbers)
 
+#numbers.each { |index| array << index ** 2 }
 
 
 #new_numbers = [9,10,16,25]
